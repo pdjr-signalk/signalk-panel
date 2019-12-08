b8f46e4d0c9b5fcc9b95052fcca6facdfdcaef27
Paul Reeve <preeve@pdjr.eu>
2019-12-08 11:10:00 +0100

    New overlay handling

--- public/lib/Overlay.js
+++ public/lib/Overlay.js
@@ -1,96 +1,68 @@
 class Overlay {
 
-    static createOverlay(source) {
+    static create(source) {
         return(new Overlay(source));
     }
 
-    constructor(source) {
-        //console.log("Overlay(%s)...", source);
+    constructor(sources) {
+        //console.log("Overlay(%s)...", sources);
 
         this.overlay = document.getElementById("overlay");
+        this.overlayTitle = document.getElementById("overlay-title");
         this.container = document.getElementById("overlay-container");
-        this.sources = (Array.isArray(source))?source:[source];
-        this.currentObject = undefined;
+        this.sources = (Array.isArray(sources))?sources:[sources];
 
         var _this = this;
-        var es = document.getElementsByClassName("overlay-close");
+        var es = this.overlay.getElementsByClassName("overlay-close");
         [...es].forEach(e => { e.addEventListener("click", function(e) { _this.close(); }); }); 
-        var es = document.getElementsByClassName("overlay-next");
-        [...es].forEach(e => { e.addEventListener("click", function(e) { _this.displaySource("next"); }); }); 
-        var es = document.getElementsByClassName("overlay-prev");
-        [...es].forEach(e => { e.addEventListener("click", function(e) { _this.displaySource("prev"); }); }); 
+        var es = this.overlay.getElementsByClassName("overlay-next");
+        [...es].forEach(e => { e.addEventListener("click", function(e) { _this.loadSource("next"); }); }); 
+        var es = this.overlay.getElementsByClassName("overlay-prev");
+        [...es].forEach(e => { e.addEventListener("click", function(e) { _this.loadSource("prev"); }); }); 
 
-        this.displaySource();
+        this.loadSource();
         this.overlay.style.display = "flex";
     }
 
-    displaySource(which=undefined) {
-        //console.log(JSON.stringify(this.sources));
+    loadSource(which=undefined) {
+        //console.log("loadSource(%s)...", which);
 
-        this.closeCurrentSource();
         if (which == "next") this.sources.push(this.sources.shift());
         if (which == "prev") this.sources.unshift(this.sources.pop());
+
+        var content = undefined;
+        var _this = this;
+
         while (this.container.firstChild) this.container.removeChild(this.container.firstChild);
-        if (typeof this.sources[0] == "function") {
-            this.currentObject = this.sources[0]();
-            this.container.appendChild(this.currentObject.getTree());
-        } else {
-            this.currentObject = undefined;
-            var object = this.container.appendChild(document.createElement("object"));
-            object.id = "overlay-oject";
-            object.style.width = "100%";
-            object.style.height = "50vh";
-            object.data = this.sources[0];
-            this.container.appendChild(object);
+        switch (typeof this.sources[0]) {
+            case "function":
+                content = this.sources[0]();
+            case "object":
+                if (content === undefined) content = this.sources[0].getTree();
+                var titles = content.querySelectorAll('[data-overlay-title]');
+                this.overlayTitle.innerHTML = (titles.length > 0)?titles[0].getAttribute("data-overlay-title"):"";
+                break;
+            case "string":
+                content = this.container.appendChild(document.createElement("object"));
+                content.id = "overlay-object";
+                content.style.width = "100%";
+                content.style.height = "50vh";
+                content.addEventListener("load", function() {
+                    var titles = content.contentDocument.getElementsByTagName("title");
+                    _this.overlayTitle.innerHTML = (titles.length > 0)?titles[0].innerHTML:"";
+                });
+                content.data = this.sources[0];
+                break;
+            default:
+                break;
         }
-
-        var titles = document.querySelectorAll('[data-overlay-title]');
-        var title = (titles.length > 0)?titles[0].getAttribute("data-overlay-title"):"***";
-        var span = document.getElementById('overlay-title');
-        while (span.firstChild) span.removeChild(span.firstChild );
-        span.appendChild(document.createTextNode(title));
-    }
-
-    closeCurrentSource() {
-        if (this.container.firstChild) this.container.removeChild(this.container.firstChild );
-        if (this.currentObject !== undefined) { this.currentObject.onClose(); this.currentObject = undefined; }
+        if (content !== undefined) this.container.appendChild(content);
     }
 
     close() {
-        var iframe = document.getElementById("overlay-object");
-        if (iframe != null) iframe.contentWindow.close();
-        this.closeCurrentSource();
+        while (this.container.firstChild) this.container.removeChild(this.container.firstChild);
+        this.overlayTitle.innerHTML = "";
         this.overlay.style.display = "none";
-    }
-
-    loadHTML(container, url, classname, callback) {
-        if (container) {
-            this.httpGetAsync(url, (content) => {
-                if (content) {
-                    container.innerHTML = content;
-                    if (classname) container.classname = classname;
-                    if (callback) callback(container);
-                }
-            });
-        }
-    }
-
-    httpGetAsync(theUrl, callback) {
-        var xmlHttp = new XMLHttpRequest();
-        xmlHttp.onreadystatechange = function() { 
-            if (xmlHttp.readyState == 4 && xmlHttp.status == 200) callback(xmlHttp.responseText);
-        }
-        xmlHttp.open("GET", theUrl, true); // true for asynchronous 
-        xmlHttp.send(null);
-    }
-
-    httpGet(theUrl) {
-        //console.log("httpGet(%s)...", theUrl);
-
-        var xmlHttp = new XMLHttpRequest();
-        xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-        xmlHttp.send( null );
-        return xmlHttp.responseText;
     }
 
 }
